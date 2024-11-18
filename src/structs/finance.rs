e70152a4e74332650a191bbb054e75398a73b281
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2024-11-18 12:22:45 +0800

    Strings instead of &str :(

--- src/structs/finance.rs
+++ src/structs/finance.rs
@@ -2,29 +2,29 @@ use serde::{Serialize, Deserialize};
 
 /// A single balance change.
 #[derive(Serialize, Deserialize)]
-pub struct Posting<'a>
+pub struct Posting
 {
 	/// The account that is being changed
-	account: &'a str,
+	account: String,
 	/// The amount that is being added or removed
 	amount: i32,
 	/// The currency that is being used.
 	/// **Currently serves no purpose**
-	currency: &'a str,
+	currency: String,
 }
 
 /// A single transaction
 #[derive(Serialize, Deserialize)]
-pub struct Transaction<'a>
+pub struct Transaction
 {
 	/// The `time::Date` the transaction happened
 	date: toml::value::Date,
 	/// A user written description for the transaction
-	description: &'a str,
+	description: String,
 	/// Who was paid, must be consistant for any filtering or cumulation functionality to work
-	payee: &'a str,
+	payee: String,
 	/// All changes in balances that occured as a result of this transaction
-	postings: Vec<Posting<'a>>,
+	postings: Vec<Posting>,
 	/// When this transaction was logged - no relation to the date it occured
 	/// Stored in UTC+0 and localised when shown to the user
 	logged: toml::value::Datetime,
