7806c4dacc0332aec139a3c41cbf36c6f4404c9e
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2024-11-20 19:06:26 +0800

    moved todos

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,3 @@ mod secondary; // Stuff to make other stuff work
 pub use structs::*;
 #[doc(inline)]
 pub use secondary::*;
-
-// TODO: Look into multithreaded reading from drive & writing to drive
-// TODO: Read & write functions (or maybe that should be a library caller problem?)
-// TODO: Optional save split into year/month/week files
-
-
